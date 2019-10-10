e4dd94c42c404ad30c88a2c267311db0afe1c380
小权 <597141997@qq.com>
2019-10-11 00:17:43 +0800

    finish promise

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const Promise = require("./promise");
-new Promise((resolve, reject)=>{
-  setTimeout(()=>{
-    resolve("aaa");
-    // reject("bbb");
-  }, 1000)
-}).then((value) => {
-  console.log(value, 'sss');
-}, (reason) => {
-  console.log(reason, 'sssaa');
+// const promise = new Promise((resolve, reject)=>{
+//   setTimeout(()=>{
+//     // resolve("aaa");
+//     reject("bbb");
+//   }, 1000)
+// })
+// const promise2 = promise.then((value) => {
+//   // return promise2;
+//   console.log(value,'ssss');
+//   // return new Promise((resolve, reject) => {
+//   //   resolve(value);
+//   // });
+//   return 'value'
+// }, (err)=>{
+//   console.log(err);
+//   return err;
+// }).then(value=>{
+//   console.log(value, 'seeess');
+// });
+const promise1 = new Promise((resolve, reject) => {
+  resolve(123);
 });
-
+const promise2 = new Promise((resolve, reject) => {
+  resolve(1234);
+});
+const promise3 = new Promise((resolve, reject) => {
+  resolve(125);
+});
+Promise.all([promise1, promise2, promise3]).then((data) => {
+  console.log(data, 'aaasss');
+});
--- promise.js
+++ promise.js
@@ -28,22 +28,104 @@ class Promise{
     }
   }
   then(onFulfilled, onRejected){
-    if(this.state === 'fulfilled'){
-      onFulfilled(this.value);
-    }
-    if(this.state === 'rejected'){
-      onRejected(this.reason);
-    }
-    if(this.state === 'pending'){
-      this.onRejectedCallbacks.push(() => {
-        onRejected(this.reason)
-      });
-      this.onResolvedCallbacks.push(()=> {
-        console.log(this.value, 'aaaa');
-        onFulfilled(this.value)
-      });
-    }
+    // 声明返回的promise2
+    let promise2 = new Promise((resolve, reject) => {
+      if(this.state === 'fulfilled'){
+        let x = onFulfilled(this.value);
+        resolvePromise(promise2, x, resolve, reject);
+      }
+      if(this.state === 'rejected'){
+        let x = onRejected(this.reason);
+        resolvePromise(promise2, x, resolve, reject);
+      }
+      if(this.state === 'pending'){
+        this.onResolvedCallbacks.push(()=>{
+          let x = onFulfilled(this.value);
+          resolvePromise(promise2, x, resolve, reject);
+        });
+        this.onRejectedCallbacks.push(() => {
+          let x = onRejected(this.reason);
+          resolvePromise(promise2, x, resolve, reject);
+        });
+      }
+    });
+    return promise2;
   }
 }
 
+function resolvePromise(promise2, x, resolve, reject){
+  if(x === promise2){
+    // reject报错
+    return reject(new TypeError('Chaining cycle detected for promise'));
+  }
+  // 防止多次调用
+  let called;
+  // x 不是null 且x是对象或者行数
+  if(x != null && (typeof x === 'object' || typeof x === 'function')){
+    try{
+      // A+规定, 声明then=x的then方法
+      let then = x.then;
+      if(typeof then === 'function'){
+        then.call(x, y => {
+          if(called) return;
+          called = true;
+          resolvePromise(promise2, y, resolve, reject);
+        }, err => {
+          if(called) return;
+          called = true;
+          reject(err);
+        });
+      }
+    } catch(e) {
+      // 输入于失败
+      if(called) return;
+      called = true;
+      reject(e);
+    }
+  }else {
+    resolve(x);
+  }
+}
+
+Promise.resolve = function(value){
+  return new Promise((resolve, reject) => {
+    resolve(value);
+  });
+}
+
+Promise.reject = function(err){
+  return new Promise((resolve, reject) => {
+    reject(err);
+  });
+}
+
+Promise.race = function(promises){
+  return new Promise((resolve, reject) => {
+    for(let i = 0; i < promises.length; ++i){
+      promises[i].then(resolve, reject);
+    }
+  });
+}
+
+Promise.all = function(promises){
+  let arr = [];
+  let i = 0;
+  function processData(index, data, resolve){
+    arr[index] = data;
+    i ++;
+
+    if(i == promises.length){
+      console.log(arr, 'sss');
+      console.log(resolve, 'sss');
+      resolve(arr);
+    }
+  }
+  return new Promise((resolve, reject) => {
+    for(let i = 0; i < promises.length; ++i){
+      promises[i].then(data=>{
+        processData(i, data, resolve);
+      }, reject);
+    }
+  });
+}
 module.exports = Promise;
